25bb4764771b0ed5cd09d2e7d01b201cafad3662
Pezhman Nasirifard <epezhman@gmail.com>
2016-08-31 16:46:12 +0200

    africa

--- run_planet.sh
+++ run_planet.sh
@@ -3,10 +3,10 @@
 
 
 
-./run_continent.sh configs/continents/africa.conf
+# ./run_continent.sh configs/continents/africa.conf
 ./run_continent.sh configs/continents/asia.conf
 ./run_continent.sh configs/continents/australiaoceania.conf
 ./run_continent.sh configs/continents/centralamerica.conf
 ./run_continent.sh configs/continents/europe.conf
-./run_continent.sh configs/continents/northamerica.conf
+# ./run_continent.sh configs/continents/northamerica.conf
 ./run_continent.sh configs/continents/southamerica.conf
--- run_planet_matlab.sh
+++ run_planet_matlab.sh
@@ -3,10 +3,10 @@
 
 
 
-./run_continent_matlab.sh configs/continents/africa.conf
+# ./run_continent_matlab.sh configs/continents/africa.conf
 ./run_continent_matlab.sh configs/continents/asia.conf
 ./run_continent_matlab.sh configs/continents/australiaoceania.conf
 ./run_continent_matlab.sh configs/continents/centralamerica.conf
 ./run_continent_matlab.sh configs/continents/europe.conf
-./run_continent_matlab.sh configs/continents/northamerica.conf
+# ./run_continent_matlab.sh configs/continents/northamerica.conf
 ./run_continent_matlab.sh configs/continents/southamerica.conf
