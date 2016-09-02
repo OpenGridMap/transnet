860976f7f31031a18942f90e5b36f06a04d655e6
Pezhman Nasirifard <epezhman@gmail.com>
2016-09-02 15:56:35 +0000

    configs

--- run_planet.sh
+++ run_planet.sh
@@ -4,9 +4,10 @@
 
 
 # ./run_continent.sh configs/continents/africa.conf
-./run_continent.sh configs/continents/asia.conf
-./run_continent.sh configs/continents/australiaoceania.conf
+#./run_continent.sh configs/continents/asia.conf
+./run_continent.sh configs/continents/russia.conf
+#./run_continent.sh configs/continents/australiaoceania.conf
 # ./run_continent.sh configs/continents/centralamerica.conf
-./run_continent.sh configs/continents/southamerica.conf
-./run_continent.sh configs/continents/europe.conf
-# ./run_continent.sh configs/continents/northamerica.conf
+#./run_continent.sh configs/continents/southamerica.conf
+#./run_continent.sh configs/continents/europe.conf
+ ./run_continent.sh configs/continents/northamerica.conf
