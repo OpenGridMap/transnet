82a9406f04701955faa7c572354d9523a2937db3
Pezhman Nasirifard <epezhman@gmail.com>
2016-09-08 22:26:36 +0000

    modeling countries of continent

--- bash/_prepare_db_planet.sh
+++ bash/_prepare_db_planet.sh
@@ -3,10 +3,10 @@
 
 
 #./prepare_db.sh ../configs/continents/africa.conf
-#./prepare_db.sh ../configs/continents/asia.conf
+./prepare_db.sh ../configs/continents/asia.conf
 #./prepare_db.sh ../configs/continents/russia.conf
 ./prepare_db.sh ../configs/continents/australiaoceania.conf
 ./prepare_db.sh ../configs/continents/centralamerica.conf
-#./prepare_db.sh ../configs/continents/southamerica.conf
+./prepare_db.sh ../configs/continents/southamerica.conf
 #./prepare_db.sh ../configs/continents/europe.conf
 #./prepare_db.sh ../configs/continents/northamerica.conf
--- bash/_run_whole_continent_topology.sh
+++ bash/_run_whole_continent_topology.sh
@@ -3,10 +3,10 @@
 
 
 #./run_continent_topology.sh ../configs/continents/africa.conf -g
-#./run_continent_topology.sh ../configs/continents/asia.conf -g
+./run_continent_topology.sh ../configs/continents/asia.conf -g
 #./run_continent_topology.sh ../configs/continents/russia.conf -g
 ./run_continent_topology.sh ../configs/continents/australiaoceania.conf -g
 ./run_continent_topology.sh ../configs/continents/centralamerica.conf -g
-#./run_continent_topology.sh ../configs/continents/southamerica.conf -g
+./run_continent_topology.sh ../configs/continents/southamerica.conf -g
 #./run_continent_topology.sh ../configs/continents/europe.conf -g
 #./run_continent_topology.sh ../configs/continents/northamerica.conf -g
