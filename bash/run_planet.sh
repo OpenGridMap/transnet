ea986592a2c4e363482f9e42521709cda7aadabd
Pezhman Nasirifard <epezhman@gmail.com>
2016-09-08 09:41:31 +0000

    modeling planet

--- bash/run_planet.sh
+++ bash/run_planet.sh
@@ -10,13 +10,13 @@ which python > ../logs/python_ver.txt
 export HTTP_PROXY="http://proxy:8080"
 export HTTPS_PROXY="https://proxy:8080"
 
-#./prepare_db_planet.sh | tee -a "../logs/planet_db.log"
+./prepare_db_planet.sh | tee -a "../logs/planet_db.log"
 
 ./prepare_planet_poly_and_voltages.sh | tee -a "../logs/planet_poly_and_voltages.log"
 
-#./run_planet_topology.sh | tee -a "../logs/planet_topology.log"
+./run_planet_topology.sh | tee -a "../logs/planet_topology.log"
 
-#./run_planet_matlab.sh | tee -a "../logs/planet_matlab.log"
+./run_planet_matlab.sh | tee -a "../logs/planet_matlab.log"
 
 cd ..
 
