957fee0bcbb2669b53a5f7dd5387dd797d181b98
Pezhman Nasirifard <epezhman@gmail.com>
2016-11-02 18:35:44 +0100

    modeling continents

--- bash/_prepare_planet_poly_and_voltages.sh
+++ bash/_prepare_planet_poly_and_voltages.sh
@@ -2,13 +2,13 @@
 # launch a complete Topology modeling for planet
 
 
-./prepare_continent_poly_and_voltages.sh ../configs/continents/africa.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/asia.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/russia.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/australiaoceania.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/centralamerica.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/southamerica.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/europe.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/africa.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/asia.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/russia.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/australiaoceania.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/centralamerica.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/southamerica.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/europe.conf -g
 ./prepare_continent_poly_and_voltages.sh ../configs/continents/germany.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/northamerica.conf -g
-./prepare_continent_poly_and_voltages.sh ../configs/continents/usa.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/northamerica.conf -g
+#./prepare_continent_poly_and_voltages.sh ../configs/continents/usa.conf -g
--- bash/_run_whole_continent_topology.sh
+++ bash/_run_whole_continent_topology.sh
@@ -2,13 +2,13 @@
 # launch a complete Topology modeling for planet
 
 
-./run_continent_topology.sh ../configs/continents/africa.conf -g
-./run_continent_topology.sh ../configs/continents/asia.conf -g
-./run_continent_topology.sh ../configs/continents/russia.conf -g
-./run_continent_topology.sh ../configs/continents/australiaoceania.conf -g
-./run_continent_topology.sh ../configs/continents/centralamerica.conf -g
-./run_continent_topology.sh ../configs/continents/southamerica.conf -g
-./run_continent_topology.sh ../configs/continents/europe.conf -g
+#./run_continent_topology.sh ../configs/continents/africa.conf -g
+#./run_continent_topology.sh ../configs/continents/asia.conf -g
+#./run_continent_topology.sh ../configs/continents/russia.conf -g
+#./run_continent_topology.sh ../configs/continents/australiaoceania.conf -g
+#./run_continent_topology.sh ../configs/continents/centralamerica.conf -g
+#./run_continent_topology.sh ../configs/continents/southamerica.conf -g
+#./run_continent_topology.sh ../configs/continents/europe.conf -g
 ./run_continent_topology.sh ../configs/continents/germany.conf -g
-./run_continent_topology.sh ../configs/continents/northamerica.conf -g
-./run_continent_topology.sh ../configs/continents/usa.conf -g
+#./run_continent_topology.sh ../configs/continents/northamerica.conf -g
+#./run_continent_topology.sh ../configs/continents/usa.conf -g
