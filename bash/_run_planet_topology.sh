db805362457d88850d61b59feb1b9af6a603e735
Pezhman Nasirifard <epezhman@gmail.com>
2016-11-16 11:55:59 +0100

    modeling continents

--- bash/_run_planet_topology.sh
+++ bash/_run_planet_topology.sh
@@ -6,9 +6,9 @@
 #./run_continent_topology.sh ../configs/continents/asia.conf
 #./run_continent_topology.sh ../configs/continents/russia.confcd ..
 #./run_continent_topology.sh ../configs/continents/australiaoceania.conf
-#./run_continent_topology.sh ../configs/continents/centralamerica.conf
+./run_continent_topology.sh ../configs/continents/centralamerica.conf
 #./run_continent_topology.sh ../configs/continents/southamerica.conf
 #./run_continent_topology.sh ../configs/continents/europe.conf
-./run_continent_topology.sh ../configs/continents/germany.conf
+#./run_continent_topology.sh ../configs/continents/germany.conf
 #./run_continent_topology.sh ../configs/continents/northamerica.conf
 #./run_continent_topology.sh ../configs/continents/usa.conf
