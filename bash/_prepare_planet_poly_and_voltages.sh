1873d48b529f16db49799806d85418732946cded
Pezhman Nasirifard <epezhman@gmail.com>
2017-02-09 12:07:52 +0100

    finding missing stations data

--- bash/_prepare_planet_poly_and_voltages.sh
+++ bash/_prepare_planet_poly_and_voltages.sh
@@ -3,12 +3,12 @@
 
 
 ./prepare_continent_poly_and_voltages.sh ../configs/continents/africa.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/asia.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/russia.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/australiaoceania.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/asia.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/russia.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/australiaoceania.conf -g
 ./prepare_continent_poly_and_voltages.sh ../configs/continents/centralamerica.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/southamerica.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/europe.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/germany.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/northamerica.conf -g
-#./prepare_continent_poly_and_voltages.sh ../configs/continents/usa.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/southamerica.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/europe.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/germany.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/northamerica.conf -g
+./prepare_continent_poly_and_voltages.sh ../configs/continents/usa.conf -g
