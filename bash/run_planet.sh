9d7e2338619a48d5a7312dcd5543444154d68c64
Pezhman Nasirifard <epezhman@gmail.com>
2017-03-30 16:34:21 +0200

    fixes

--- bash/run_planet.sh
+++ bash/run_planet.sh
@@ -20,25 +20,27 @@ which python > ../logs/python_ver.txt
 export HTTP_PROXY="http://proxy:8080"
 export HTTPS_PROXY="https://proxy:8080"
 
-cat /dev/null > ../logs/planet_db.log
-cat /dev/null > ../logs/planet_poly_and_voltages.log
-cat /dev/null > ../logs/planet_topology.log
-cat /dev/null > ../logs/planet_matlab.log
-cat /dev/null > ../logs/whole_continent_topology.log
-cat /dev/null > ../logs/whole_continent_matlab.log
+mkdir -p "../../transnet-models/logs"
+
+cat /dev/null > ../../transnet-models/logs/planet_db.log
+cat /dev/null > ../../transnet-models/logs/planet_poly_and_voltages.log
+cat /dev/null > ../../transnet-models/logs/planet_topology.log
+cat /dev/null > ../../transnet-models/logs/planet_matlab.log
+cat /dev/null > ../../transnet-models/logs/whole_continent_topology.log
+cat /dev/null > ../../transnet-models/logs/whole_continent_matlab.log
 
 
-./_prepare_db_planet.sh | tee -a "../logs/planet_db.log"
+#./_prepare_db_planet.sh | tee -a "../../transnet-models/logs/planet_db.log"
 
-./_prepare_planet_poly_and_voltages.sh | tee -a "../logs/planet_poly_and_voltages.log"
+./_prepare_planet_poly_and_voltages.sh | tee -a "../../transnet-models/logs/planet_poly_and_voltages.log"
 
-./_run_planet_topology.sh | tee -a "../logs/planet_topology.log"
+./_run_planet_topology.sh | tee -a "../../transnet-models/logs/planet_topology.log"
 
-#./_run_planet_matlab.sh | tee -a "../logs/planet_matlab.log"
+#./_run_planet_matlab.sh | tee -a "../../transnet-models/logs/planet_matlab.log"
 
-./_run_whole_continent_topology.sh | tee -a "../logs/whole_continent_topology.log"
+./_run_whole_continent_topology.sh | tee -a "../../transnet-models/logs/whole_continent_topology.log"
 
-#./_run_whole_continent_matlab.sh | tee -a "../logs/whole_continent_matlab.log"
+#./_run_whole_continent_matlab.sh | tee -a "../../transnet-models/logs/whole_continent_matlab.log"
 
 cd ../../transnet-models
 git add .
